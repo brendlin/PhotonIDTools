183e3d106746d509fc45c1fad8375763c7db23c8
Kurt Brendlinger <kurt.brendlinger@cern.ch>
2018-07-16 18:04:58 +0200

    Add makePicoXaod conf file for SinglePhoton sample

---
+++ data/makePicoXaod_SinglePhotonConf.py
@@ -0,0 +1,30 @@
+
+treename = 'SinglePhoton'
+
+bkgs = '\
+PyPt17_inf_mc16a_v21,\
+PyPt17_inf_mc16d_v21,\
+'
+
+cuts = [
+    ]
+
+variables = {
+    'mcTotWeightNoPU_PIDuse',
+    'y_isTruthMatchedPhoton',
+    'y_convType',
+    'y_pt',
+    'y_eta_cl_s2',
+    'y_e277',
+    'y_f1',
+    'y_Rhad',
+    'y_Rhad1',
+    'y_Reta',
+    'y_Rphi',
+    'y_weta2',
+    'y_weta1',
+    'y_wtots1',
+    'y_fracs1',
+    'y_deltae',
+    'y_Eratio',
+   }
